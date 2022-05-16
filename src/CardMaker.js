f4279522f23fbe71f8cebfdad2bc07bd79ceb7a5
Seth Feldman <namdlefhtes@gmail.com>
2022-05-16 08:39:05 -0400

    Last working build

--- src/CardMaker.js
+++ src/CardMaker.js
@@ -5,13 +5,20 @@ export default function CardMaker(name, bio) {
     const div = document.createElement("div");
     const h4 = document.createElement("h4");
     const p = document.createElement("p");
+
+    let loader = document.getElementsByClassName("loader")[0];
     // remove loader
-    document.getElementsByClassName("loader")[0].remove();
-    // create bio card
+    if( loader) {
+        loader.remove();
+            // create bio card
     div.classList.add("bio-card");
     main.append(div);
     div.append(h4);
     div.append(p);
     h4.innerText = name;
     p.innerText = bio;
+
+    }
+ 
+
 };
--- src/FetchAPI.js
+++ src/FetchAPI.js
@@ -1,14 +1,15 @@
 import CardMaker from "./CardMaker";
 import Loader from "./Loader";
+import { config } from "../config";
 
 export default function FetchAPI(textInput) {
 
-    const apiKey = `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`;
-    const engineId= `text-curie-001`;
+    const apiKey = config.key;
+    const engineId= `text-davinci-002`;
     const URI = `https://api.openai.com/v1/engines/${engineId}/completions`;
 
     const data = {
-      prompt: `Tell me the origin story of the following musical artist: \n: ${textInput}`,
+      prompt: `Tell me the origin story of the following musical artist: ${textInput}`,
       max_tokens: 200,
       temperature: 1,
     };
@@ -25,11 +26,13 @@ export default function FetchAPI(textInput) {
       }) 
       .then(response => response.json())
       .then(result => {  
+          console.log(result);
           let bio = result.choices[0].text; 
           CardMaker(textInput, bio);
           })
       .catch(error => {
         console.log("error, please try again");
+        document.getElementsByClassName("loader")[0].innerText = `Error: API not responding. OpenAI may be experiencing an outage at this time.`
       })    
   }
 
